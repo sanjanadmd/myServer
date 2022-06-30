62713b9c128cf5d4ac5970f1c23457af81eb785d
sanjanadondapati <99525594+sanjanadmd@users.noreply.github.com>
2022-06-30 12:14:46 +0530

    Fixed the path issues

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { myServer } = require('./src/server/myServer.js');
-const { createHandler } = require('./src/router.js');
-const { serveStaticFile } = require('./src/serveStaticFile.js');
+const { startServer } = require('./src/server/myServer.js');
+const { createHandler } = require('./src/server/router.js');
+const { serveStaticFile } = require('./src/handlers/serveStaticFile.js');
 
 module.exports = {
-  myServer,
+  startServer,
   createHandler,
   serveStaticFile
 };
