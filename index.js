7232a13f8c9de74c46deeb68a1d8b5f1603c3da3
sanjanadondapati <99525594+sanjanadmd@users.noreply.github.com>
2022-07-01 09:43:59 +0530

    Updated exported modules

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { startServer } = require('./src/server/myServer.js');
 const { createHandler } = require('./src/server/router.js');
 const { bodyParser } = require('./src/handlers/bodyParser.js');
+const { notFoundHandler } = require('./src/handlers/notFound.js');
 const { serveStaticFile } = require('./src/handlers/serveStaticFile.js');
 
 module.exports = {
-  bodyParser,
   startServer,
   createHandler,
-  serveStaticFile
+  bodyParser,
+  serveStaticFile,
+  notFoundHandler
 };
