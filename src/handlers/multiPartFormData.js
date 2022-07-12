3738fad23c429b42b8fd09f9d88a87c79857fd1c
sanjanadondapati <99525594+sanjanadmd@users.noreply.github.com>
2022-07-12 15:49:51 +0530

    Updated multiPartFormData for parsing multiple fields

--- src/handlers/multiPartFormData.js
+++ src/handlers/multiPartFormData.js
@@ -1,13 +1,14 @@
 const CRLF = '\r\n';
 
 const lowercase = value => value.trim().toLowerCase();
+const formatValue = (value) => value.trim().replaceAll('"', '');
 
 const parseAttributes = (attributes) => {
   const parsedAttributes = {};
 
   attributes.forEach(attribute => {
     const [key, value] = attribute.split('=');
-    parsedAttributes[lowercase(key)] = value;
+    parsedAttributes[lowercase(key)] = formatValue(value);
   });
 
   return parsedAttributes;
@@ -22,29 +23,36 @@ const parseHeaders = (headerString) => {
     }
     const [rawHeader, ...rawAttributes] = line.split(';');
 
-    const [header, value] = rawHeader.split(':');
+    const [header, headerValue] = rawHeader.split(':');
     const attributes = parseAttributes(rawAttributes);
 
-    headers[lowercase(header)] = { value, attributes };
+    headers[lowercase(header)] = { value: headerValue?.trim(), attributes };
   });
 
   return headers;
 };
 
 const parseMultipartFormData = (data, boundary) => {
+  const params = [];
+  let boundaryStart = 1;
 
-  const boundaryStart = data.indexOf(boundary) + boundary.length;
-  const boundaryEnd = data.lastIndexOf(`${CRLF}--${boundary}`);
+  let content = data;
+  while (boundaryStart > 0) {
+    boundaryStart = content.indexOf(boundary);
+    const nextBoundary = content.slice(boundaryStart).indexOf(`${CRLF}--${boundary}`) + boundaryStart;
 
-  const content = data.slice(boundaryStart, boundaryEnd);
-  const contentIndex = content.indexOf(CRLF.repeat(2));
+    const actualContent = content.slice(boundaryStart + boundary.length, nextBoundary);
+    content = content.slice(nextBoundary + 1);
+    const contentIndex = actualContent.indexOf(CRLF.repeat(2));
 
-  const rawHeaders = content.slice(0, contentIndex + 4);
-  const body = content.slice(contentIndex + 4);
+    const rawHeaders = actualContent.slice(0, contentIndex + 4);
+    const body = actualContent.slice(contentIndex + 4);
 
-  const headers = parseHeaders(rawHeaders.toString());
+    const headers = parseHeaders(rawHeaders.toString());
+    params.push({ headers, body });
+  }
 
-  return { headers, body };
+  return params;
 };
 
 module.exports = { parseMultipartFormData };
